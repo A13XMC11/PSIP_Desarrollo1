f41bd0b60f9591088cead1758cb3c37279d159a9
Joel Otavalo <joeferota14@gmail.com>
2026-07-06 18:46:38 -0500

    Actualiza entities: Auditoria, ReporteDiario y Rol

--- src/main/java/com/uisrael/asistencia/infraestructura/persistencia/jpa/AuditoriaEntity.java
+++ src/main/java/com/uisrael/asistencia/infraestructura/persistencia/jpa/AuditoriaEntity.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -24,5 +26,9 @@ public class AuditoriaEntity {
 	private String detalle;
 	private String ip;
 	private LocalDateTime fechaHora;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_empleado")
+	private EmpleadoEntity fkEmpleadoEntity;
 	
 }
--- src/main/java/com/uisrael/asistencia/infraestructura/persistencia/jpa/RolEntity.java
+++ src/main/java/com/uisrael/asistencia/infraestructura/persistencia/jpa/RolEntity.java
@@ -1,12 +1,14 @@
 package com.uisrael.asistencia.infraestructura.persistencia.jpa;
 
 import java.time.LocalDateTime;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -22,5 +24,8 @@ public class RolEntity {
 	private String nombre;
 	private String descripcion;
 	private LocalDateTime creadoRol;
+	
+	@OneToMany(mappedBy = "fkRolEntity")
+	private List<EmpleadoEntity> listaEmpleados = new ArrayList<>();
 	
 }
