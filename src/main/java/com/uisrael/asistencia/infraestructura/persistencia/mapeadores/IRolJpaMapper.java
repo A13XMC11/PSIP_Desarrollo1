7c3445a1ecf5479642e99a23a83f7e89060daaa5
Joel Otavalo <joeferota14@gmail.com>
2026-07-20 21:28:45 -0500

    update: IRolJpaMapper

--- src/main/java/com/uisrael/asistencia/infraestructura/persistencia/mapeadores/IRolJpaMapper.java
+++ src/main/java/com/uisrael/asistencia/infraestructura/persistencia/mapeadores/IRolJpaMapper.java
@@ -2,14 +2,18 @@ package com.uisrael.asistencia.infraestructura.persistencia.mapeadores;
 
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.uisrael.asistencia.dominio.entidades.Rol;
 import com.uisrael.asistencia.infraestructura.persistencia.jpa.RolEntity;
 
 @Mapper(componentModel = "spring")
 public interface IRolJpaMapper {
-
+	@Mapping(target = "nombreRol", source = "nombre")
+	@Mapping(target = "descripcionRol", source = "descripcion")
 	Rol toDomain(RolEntity entity);
 	
+	@Mapping(target = "nombre", source = "nombreRol")
+	@Mapping(target = "descripcion", source = "descripcionRol")
 	RolEntity toEntity(Rol RolPojo);
 }
