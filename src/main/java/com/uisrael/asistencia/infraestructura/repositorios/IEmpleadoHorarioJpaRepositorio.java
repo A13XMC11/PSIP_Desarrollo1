4045703239b5ac16c4727e4dd5eaf59790ef722b
Alexander Mejia <99159842+A13XMC11@users.noreply.github.com>
2026-07-20 23:00:44 -0500

    fix: arreglo de Query dentro de EmpleadoHorarioJpaRepositorio

--- src/main/java/com/uisrael/asistencia/infraestructura/repositorios/IEmpleadoHorarioJpaRepositorio.java
+++ src/main/java/com/uisrael/asistencia/infraestructura/repositorios/IEmpleadoHorarioJpaRepositorio.java
@@ -8,10 +8,10 @@ import org.springframework.data.jpa.repository.Query;
 import com.uisrael.asistencia.infraestructura.persistencia.jpa.EmpleadoHorarioEntity;
 
 public interface IEmpleadoHorarioJpaRepositorio extends JpaRepository<EmpleadoHorarioEntity, Integer> {
-	@Query("Select eh from EmpleadoHorarioEntity eh where eh.fkEmpleadoEntity.idEmpleado=?1 and eh.estadoEmpleadoHorario=true")
+	@Query("Select eh from EmpleadoHorarioEntity eh where eh.idEmpleado=?1 and eh.estadoEmpleadoHorario = true")
 	List<EmpleadoHorarioEntity> buscarHorariosActivosPorEmpleado(int idEmpleado);
 
-	@Query("Select eh from EmpleadoHorarioEntity eh where eh.fkHorarioEntity.idHorario=?1")
+	@Query("Select eh from EmpleadoHorarioEntity eh where eh.idHorario=?1")
 	List<EmpleadoHorarioEntity> buscarPorHorario(int idHorario);
 
 }
